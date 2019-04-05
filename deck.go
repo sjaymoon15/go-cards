302acb3d14d557243c7d8064071a908b6b3fe13c
Seungjae Moon <sjaymoon15@gmail.com>
2019-04-05 11:32:13 -0700

    Create a newDeck method.

--- deck.go
+++ deck.go
@@ -6,10 +6,24 @@ import "fmt"
 // whixh is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spaces", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			card := value + " of " + suit
+			cards = append(cards, card)
+		}
+	}
+	return cards
+}
+
 // receiver d
 // any variable type deck has access to this method.
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
