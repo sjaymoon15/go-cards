a331129fe07bc14ad56022ea5afa30a052c74bc8
Seungjae Moon <sjaymoon15@gmail.com>
2019-04-05 11:46:03 -0700

    Add deal method.

--- deck.go
+++ deck.go
@@ -23,7 +23,11 @@ func newDeck() deck {
 // receiver d
 // any variable type deck has access to this method.
 func (d deck) print() {
-	for _, card := range d {
-		fmt.Println(card)
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
