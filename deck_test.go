66f4bddf5bd8643544e79153c11683eddf629408
Seungjae Moon <sjaymoon15@gmail.com>
2019-04-13 16:54:01 -0700

    Add more testing to TestNewDeck func test.

--- deck_test.go
+++ deck_test.go
@@ -6,6 +6,15 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 
 	if len(d) != 16 {
-		t.Errorf("Error. Expected 20 but got %v", len(d))
+		t.Errorf("Expected 20 but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spaces" {
+		t.Errorf("Expected Ace of Spaces for the first element of deck but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected Four of Clubs for the last element of deck but got %v", d[len(d)-1])
+	}
+
 }
